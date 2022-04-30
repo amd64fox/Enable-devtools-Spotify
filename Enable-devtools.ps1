3a53070068ea30b7d822468022d9001b2de2c5d3
amd64fox <62529699+amd64fox@users.noreply.github.com>
2022-04-30 05:17:43 +0300

    Add files via upload

---
+++ Enable-devtools.ps1
@@ -0,0 +1,5 @@
+﻿$pathIn = "$env:LOCALAPPDATA\Spotify\offline.bnk"
+$ANSI = [Text.Encoding]::GetEncoding(1251)
+$old = [IO.File]::ReadAllText($pathIn, $ANSI)
+$new = $old -replace "(?<=app-developer..|app-developer>)0", '2'
+[IO.File]::WriteAllText($pathIn, $new, $ANSI)
