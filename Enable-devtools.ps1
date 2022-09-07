8aeca08345eff14a321ab42f8f97eece37645bda
amd64fox <amd64fox1@gmail.com>
2022-09-07 16:21:28 +0300

    Update
     - added Debug Tools
     - added item for employees in the Spotify settings

--- Enable-devtools.ps1
+++ Enable-devtools.ps1
@@ -4,20 +4,94 @@ Stop-Process -Name Spotify
 
 $offline_bnk = "$env:LOCALAPPDATA\Spotify\offline.bnk"
 $spotify_exe = "$env:APPDATA\Spotify\Spotify.exe"
-$spotiCheck = (Test-Path -LiteralPath $offline_bnk)
-$spotiCheck2 = (Test-Path -LiteralPath $spotify_exe)
+$xpui_spa_patch = "$env:APPDATA\Spotify\Apps\xpui.spa"
+$xpui_spa_copy = "$env:APPDATA\Spotify\Apps\xpui.spa.adminbak"
+$xpui_js_patch = "$env:APPDATA\Spotify\Apps\xpui\xpui.js"
+$xpui_js_copy = "$env:APPDATA\Spotify\Apps\xpui\xpui.js.adminbak"
+$xpui_spaCheck = (Test-Path -LiteralPath $xpui_spa_patch)
+$bnkCheck = (Test-Path -LiteralPath $offline_bnk)
+$spotiCheck = (Test-Path -LiteralPath $spotify_exe)
+$xpui_jsCheck = (Test-Path -LiteralPath $xpui_js_patch)
+$debugTools = '(return ).{1,3}(\?.{1,4}createElement\(.{3,7}{displayText:"Debug Tools")' , '$1true$2'
+$employee = '(..\(.\))(\?..createElement\(.{1,3},{filterMatchQuery:.{2,15}\("settings.employee"\))', 'true$2'
 
-if ($spotiCheck -and $spotiCheck2) {
-    $offline_bnk = "$env:LOCALAPPDATA\Spotify\offline.bnk"
+if (!($spotiCheck)) {
+
+    Write-Host "Spotify not found"`n
+    exit
+}
+if ($bnkCheck) {
     $ANSI = [Text.Encoding]::GetEncoding(1251)
     $old = [IO.File]::ReadAllText($offline_bnk, $ANSI)
-    $new = $old -replace "(?<=app-developer..|app-developer>)0", '2'
+    $new = $old -replace '(?<=app-developer..|app-developer>)0', '2'
     [IO.File]::WriteAllText($offline_bnk, $new, $ANSI)
-    Start-Process -FilePath $spotify_exe
 }
 
 else {
-
-    Write-Host 'Spotify not found'
+    Write-Host "offline.bnk not found"`n
+    Write-Host "Seems like Spotfiy hasn't been launched yet, you need to login to your account `nwait for Spotfiy to fully launch, then close it and run the script again"`n
     pause
+    exit
+}
+
+if ($xpui_jsCheck -and $xpui_spaCheck) { 
+    Write-Host "Location of Spotify files is broken, reinstall Spotify"
+    Write-Host "Part of the DevTools functionality is not activated"`n
+    Start-Process -FilePath $spotify_exe
+    pause
+    exit
+}
+if (!($xpui_jsCheck) -and !($xpui_spaCheck)) { 
+    Write-Host "xpui system files not found, reinstall Spotify"`n
+    pause
+    exit
+}
+
+if ($xpui_spaCheck) {
+
+    copy-Item $xpui_spa_patch $xpui_spa_copy
+
+    Add-Type -Assembly 'System.IO.Compression.FileSystem'
+    $zip = [System.IO.Compression.ZipFile]::Open($xpui_spa_patch, 'update')
+    $entry_xpui = $zip.GetEntry('xpui.js')
+    $reader = New-Object System.IO.StreamReader($entry_xpui.Open())
+    $xpui_js = $reader.ReadToEnd()
+    $reader.Close()
+
+    $xpui_js = $xpui_js -replace $debugTools[0], $debugTools[1] -replace $employee[0] , $employee[1]
+
+    $writer = New-Object System.IO.StreamWriter($entry_xpui.Open())
+    $writer.BaseStream.SetLength(0)
+    $writer.Write($xpui_js)
+    $writer.Close()
+    $zip.Dispose()
+
+    Start-Process -FilePath $spotify_exe
+
+    Start-Sleep -Milliseconds 1500
+
+    Remove-item $xpui_spa_patch -Recurse -Force
+    Rename-Item -path $xpui_spa_copy -NewName $xpui_spa_patch
+}
+
+if ($xpui_jsCheck) {
+
+    copy-Item $xpui_js_patch $xpui_js_copy
+
+    $reader = New-Object -TypeName System.IO.StreamReader -ArgumentList $xpui_js_patch
+    $xpui_js = $reader.ReadToEnd()
+    $reader.Close()
+
+    $xpui_js = $xpui_js -replace $debugTools[0], $debugTools[1] -replace $employee[0] , $employee[1]
+    $writer = New-Object System.IO.StreamWriter -ArgumentList $xpui_js_patch
+    $writer.BaseStream.SetLength(0)
+    $writer.Write($xpui_js)
+    $writer.Close()
+
+    Start-Process -FilePath $spotify_exe
+
+    Start-Sleep -Milliseconds 1500
+
+    Remove-item $xpui_js_patch -Recurse -Force
+    Rename-Item -path $xpui_js_copy -NewName $xpui_js_patch
 }
--- README.md
+++ README.md
@@ -1,7 +1,42 @@
-## Enable-devtools Spotify for Windows
+<p align="center">
+      <a href="https://t.me/spotify_windows_discussing"><img src="https://raw.githubusercontent.com/amd64fox/Rollback-Spotify/main/.github/Pic/Shields/telegram_group.svg"></a>
+      <a href="https://cutt.ly/8EH6NuH"><img src="https://raw.githubusercontent.com/amd64fox/Rollback-Spotify/main/.github/Pic/Shields/excel.svg"></a>
+      </p>
 
-Starting from version 1.1.80.699, access to devtools was limited, this script turns on devtools forcibly, after restarting the client, it is highly likely that the script will need to be restarted.
+<center>
+    <h1 align="center">Enable-devtools Spotify for Windows</h1>
+</center>
+
+<h2>What does the script do?</h2>
+
+- <strong>Activates the main developer options in the menu</strong>
+- <strong>Activates the developer menu on the right mouse click</strong>
+- <strong>Activates Debug Tools</strong> *(Only works with version 1.1.92 and above)*
+- <strong>Activates the item for employees in the Spotify settings</strong>
+- <strong>After closing the client, all developer menus will be disabled</strong>
 
 Just download and run [Enable-devtools.bat](https://cutt.ly/bGYVkFd)
+
+<h2>A little more</h2>
+
+<details>
+<summary><small>Screenshots</small></summary><p>
+  
+  | Main developer options in the menu | Developer menu on the right mouse click |
+| :----: | :----: |
+| ![Снимок экрана 2022-09-07 152432](https://user-images.githubusercontent.com/62529699/188879952-31daa918-5eb7-4e90-914e-99a38fb856f3.jpg) | ![Снимок экрана 2022-09-07 154755](https://user-images.githubusercontent.com/62529699/188881902-cef30290-a440-40d4-ac50-43d266cd6355.jpg)
+
+| Debug Tools | Debug Tools |
+| :----: | :----: |
+| ![Снимок экрана 2022-09-07 152032](https://user-images.githubusercontent.com/62529699/188882504-a3c0f836-8ad5-4db6-9c0a-c53846a9d797.jpg) | ![Снимок экрана 2022-09-07 155426](https://user-images.githubusercontent.com/62529699/188883988-39e4bd34-1a83-4527-878f-080e9ec415be.jpg)
+
+| Item for employees in the Spotify settings |
+| :----: |
+| ![Снимок экрана 2022-09-07 152114](https://user-images.githubusercontent.com/62529699/188884736-6eb16372-ea0c-43e3-8255-46e77fc38e19.jpg)
+
+
+</details>
+
+Starting from version 1.1.80.699, access to devtools was limited, this script turns on devtools forcibly, after restarting the client, it is highly likely that the script will need to be restarted.
 
 The idea was spied [here](https://gist.github.com/PhilippIRL/97908aba3a78cc0c8d0ab4e9439bf445)
